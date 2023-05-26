112e1f9092ab93ef23cb73be59d0976bf0583b38
THARUN C <115558955+Tharun-C@users.noreply.github.com>
2023-05-26 21:38:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Carousel from "./components/Carousel";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Carousel></Carousel>
+    </div>
+  );
+}
+
+export default App;
